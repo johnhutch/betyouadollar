8eff2515ead0d379c070a1570b1ce525a5ecfff9
John Hutchinson <johnhutch@swiftkickweb.com>
2024-10-04 18:32:44 -0400

    Changed the app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "jenry_production"
+  # config.active_job.queue_name_prefix = "betyouadollar_production"
 
   config.action_mailer.perform_caching = false
 
