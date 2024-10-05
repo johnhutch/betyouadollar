2f8bfdb111e40d35426a0e62f4ec7a4a790cd70c
John Hutchinson <johnhutch@swiftkickweb.com>
2024-10-04 20:22:04 -0400

    Got open bets displaying as cards

--- app/helpers/bets_helper.rb
+++ app/helpers/bets_helper.rb
@@ -1,2 +1,13 @@
 module BetsHelper
+  def acceptance_text
+    [
+    "You're on",
+    "I'll take that action",
+    "Sure, why not",
+    "Deal",
+    "Easy money",
+    "A fool and his $1...",
+    "*handshakeemoji*"
+    ].sample
+  end
 end
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,4 +1,14 @@
 class Bet < ApplicationRecord
   belongs_to :owner, :class_name => 'User'
   belongs_to :better, :class_name => 'User', optional: true
+
+  scope :unexpired, -> { where(expiration: Time.zone.now.. ) }
+  scope :expired, -> { where(expiration: ...Time.zone.now) }
+  scope :unaccepted, -> { where(better: nil) }
+  scope :accepted, -> { where.not(better: nil) }
+  scope :settled, -> { where(settled: true) }
+
+  def unaccepted?
+    self.better == nil
+  end
 end
